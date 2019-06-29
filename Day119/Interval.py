ebd3913d15dde8407ca32d110e07a90a40384e80
DL1683 <dl1683@rit.edu>
2019-06-30 00:07:56 +0530

    Finished the problem?
    Seems to be working but the example is giving me the wrong input

--- Day119/Interval.py
+++ Day119/Interval.py
@@ -1,23 +1,41 @@
-def covering(intervals):
-    intervals.sort(key=lambda x: x[0])
-
-    result = []
-    i = 0
-    print(intervals)
-    while i < len(intervals):
-        interval = intervals[i]
-        print(i)
-        while i < len(intervals) and intersecting(intervals[i], interval):
-            interval = (max(intervals[i][0], interval[0]), min(intervals[i][1], interval[1]))
-            i += 1
-            print("i incremented")
-        result.append(interval[1])
-        print("result:", result)
-    return result
+def findIntersection(intervals): 
+    """
+    find the intersection of a list of intersections.
+    """
+  
+    # First interval 
+    l = intervals[0][0] #lower component
+    r = intervals[0][1] #higher component
+  
+    # Check rest of the intervals  
+    # and find the intersection 
+    for i in range(1,len(intervals)): 
+        interval=[l,r]
+        # If no intersection exists 
+        if (intersecting(intervals[i],interval)):  
+            print() 
+  
+        # Else update the intersection 
+        else: 
+            l = max(l, intervals[i][0])
+            r = min(r, intervals[i][1]) 
+            interval=[l,r]
+  
+    return([l, r]) 
 
 def intersecting(x, y):
-    print(x,":x")
-    print(y,":y")
-    return not (x[0] > y[1] or y[0] > x[1])
+   """
+   Return a boolean indicaing if 2 intervals (x,y) are intersecting
+   """
+   return(y[0] > x[1] or x[0] > y[1])
 
-print(covering([[0, 3], [2, 6], [3, 4], [6, 9]]))
+l=[[0, 3], [2, 6], [3, 4], [6, 9]]
+#print(findIntersection(l)
+
+intervals= [ 
+            [ 1, 6 ], 
+            [ 2, 8 ], 
+            [ 3, 10 ], 
+            [ 5, 8 ] 
+            ] 
+print(findIntersection(intervals))
