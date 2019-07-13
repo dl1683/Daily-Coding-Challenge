192a42e0c64ec8bddbef2b4e423e865299ed8c2d
DL1683 <dl1683@rit.edu>
2019-07-14 00:13:38 +0530

    finished this problem
    Done and dusted in python

---
+++ Day9/Sum.py
@@ -0,0 +1,21 @@
+def largestSum(arr):
+    """find the largest sum of non adjacent numbers in array arr
+    @return: sum
+    """
+    if len(arr) <= 2:
+        return max(0, max(arr))
+
+    max_excluding_last= max(0, arr[0])
+    max_including_last = max(max_excluding_last, arr[1])
+
+    for num in arr[2:]:
+        prev_max_including_last = max_including_last
+
+        max_including_last = max(max_including_last, max_excluding_last + num)
+        max_excluding_last = prev_max_including_last
+
+    return max(max_including_last, max_excluding_last)
+
+
+arr=[5, 1, 1, 5]
+print(largestSum(arr))
